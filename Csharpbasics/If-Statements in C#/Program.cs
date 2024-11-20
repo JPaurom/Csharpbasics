42fc69bac003adfec7dbcf5ab62ffadc9ad748f1
JPaurom <jephthahpaurom@gmail.com>
2024-11-20 13:29:07 +0200

    Add files via upload

---
+++ Csharpbasics/If-Statements in C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statements_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write down your age to check if you are eligible to vote");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You are eligible to vote.");
+            }
+            else 
+            {
+                Console.WriteLine("You are not eligible to vote. ");
+            }
+            Console.ReadLine();
+              
+        }
+    }
+}
