a8097c4f5a279ff424182f6ce4c93c3bc6fc028e
JPaurom <jephthahpaurom@gmail.com>
2024-11-28 09:50:56 +0200

    Add files via upload

---
+++ Csharpbasics/Odd or Even C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_or_Even_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Please enter a number: ");
+            number=int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Number is even");
+}
+            else
+            {
+                Console.WriteLine("Number is odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
