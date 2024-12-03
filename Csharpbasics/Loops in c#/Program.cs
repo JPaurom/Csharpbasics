c787567b8713d44c476053576a7a2f123677c112
JPaurom <jephthahpaurom@gmail.com>
2024-12-03 10:49:51 +0200

    Add files via upload

---
+++ Csharpbasics/Loops in c#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_in_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many times should the loop run?");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+            if (loopCounter <= 0)
+            {
+                Console.WriteLine("Enter a value above 0");
+            }
+            else
+            {
+                for (int i = 1; i <= loopCounter; i++)
+                {
+                    Console.WriteLine("Current loop counter value:" + i);
+                }
+                Console.WriteLine("The loop has finished.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
