a4b521f920b1592d1c9d405698151c5671c13a51
JPaurom <jephthahpaurom@gmail.com>
2024-12-03 10:40:11 +0200

    Add files via upload

---
+++ Csharpbasics/Integer Array Task in C#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer_Array_Task_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 1, 2, 3, 4, 5 };
+
+            int sum = SumOfNumbers(numbers);
+
+            Console.WriteLine("Array Length:" + numbers.Length);
+
+            if (sum > -1)
+            {
+                Console.WriteLine($"The total is:{sum}");
+            }
+            else
+            {
+                Console.WriteLine("Cannot add up an empty array");
+            }
+
+            if (sum < 0)
+            {
+                Console.WriteLine("Canot add up an empty array");
+            }
+        }
+
+        static int SumOfNumbers(int[] arr)
+        {
+            int sum = 0;
+            foreach (int num in arr)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Csharpbasics/Sort Reverse Remove and Find in C#/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Reverse_Remove_and_Find_in_C_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 45, 12, 78, 34, 89, 23 };
+            Console.WriteLine("Original Array:");
+            PrintArray(numbers);
+
+            Array.Sort(numbers);
+            Console.WriteLine("\nSorted Array (Ascending Order):");
+            PrintArray(numbers);
+
+            Array.Reverse(numbers);
+            Console.WriteLine("\nReversed Array:");
+            PrintArray(numbers);
+
+            numbers = numbers.Where(num => num != 78).ToArray();
+            Console.WriteLine("\nArray after removing 78:");
+            PrintArray(numbers);
+
+            int index = Array.IndexOf(numbers, 34);
+            if (index == -1)
+            {
+                Console.WriteLine("\nNumber 34 does not exist in the array.");
+            }
+            else
+            {
+                Console.WriteLine($"\nIndex of number 34: {index}");
+            }
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
