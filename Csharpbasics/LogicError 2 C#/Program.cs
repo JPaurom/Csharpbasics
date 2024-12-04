32234c30555e78b0b1dd22783ac0f67cafff5cd9
JPaurom <jephthahpaurom@gmail.com>
2024-12-04 18:11:56 +0200

    Add files via upload

---
+++ Csharpbasics/LogicError 2 C#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicError_2_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            string input = Console.ReadLine();
+
+            int age;
+            if (int.TryParse(input, out age))
+            {
+
+                if (age > 50) 
+                {
+                    Console.WriteLine("Over 50");
+                }
+                else if (age > 30)
+                {
+                    Console.WriteLine("Over 30");
+                }
+                else if (age > 18)
+                {
+                    Console.WriteLine("Over 18");
+                }
+                else
+                {
+                    Console.WriteLine("18 or younger");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
