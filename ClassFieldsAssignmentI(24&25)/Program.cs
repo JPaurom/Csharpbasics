3f6e27f1e92619878c2c944b9ada33b03006108c
JPaurom <jephthahpaurom@gmail.com>
2024-12-11 13:29:59 +0200

    Add files via upload

---
+++ ClassFieldsAssignmentI(24&25)/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFieldsAssignmentI_24_25_
+{
+    class Product
+    {
+        //Private fields to store product with name, price and quantity
+        private string name;
+        private double price;
+        private int stock;
+
+        //Constructor 
+        public Product(string name, double price, int stock)
+        {
+            this.name = name; // Assign the product name
+            SetPrice(price); // Validate and set the product price
+            SetStock(stock);  //Validate and set the stock quantity
+
+        }
+        public void SetName(string name)
+        {
+            //Assign a valid name or default to "Invalid name" if input is null or empty
+            this.name = !string.IsNullOrEmpty(name) ? name : "Invalid Name";
+        }
+
+        //Method to get the name of the product
+        public string GetName()
+        {
+            //Return the name of the product
+            return name;
+        }
+        //Method to set the price of the product
+        public void SetPrice(double price)
+        { 
+            //Assign a positive price or default to 0 if input is invalid
+            this.price = price > 0 ? price : 0; 
+        }
+        //Method to get the price of the product
+        public double GetPrice()
+        {
+            //Return the price of the product
+            return price;
+        }
+        //Method to set the quantity of the product in stock
+        public void SetStock(int stock)
+        { 
+            //Assign a non negative value or default to 0 if input is invalid
+            this.stock = stock >= 0 ? stock : 0;
+        }
+        //Method to get the quantity of the product in stock
+        public int GetStock()
+        {
+            // return the stock quantity
+            return stock;
+        }
+
+        //Method to return the details of the product
+        public string ReturnDetails()
+        {
+            //return the details about the product "Name, price, stock"
+            return $"Name: {name}\nPrice: {price}$\nStock: {stock}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an instance of the product class
+            Product product = new Product("Laptop", 999.99, 20);
+            Console.WriteLine(product.ReturnDetails());
+            //Update product details
+            product.SetName("Smartphone");
+            product.SetPrice(599.99);
+            product.SetStock(20);
+
+            //Display product details
+            Console.WriteLine(product.ReturnDetails());
+            Console.ReadLine();
+              
+        }
+    }
+}
---
+++ ClassFieldsAssignmentI(24&25)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClassFieldsAssignmentI(24&25)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassFieldsAssignmentI(24&25)")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eb0202d4-f2fd-4484-8964-b4615f2220a1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
