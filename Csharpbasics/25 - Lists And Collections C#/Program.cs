c3086da88f81fc759ecb883cf4c6893a80f8729a
JPaurom <jephthahpaurom@gmail.com>
2024-12-12 09:51:26 +0200

    Update Program.cs

--- Csharpbasics/25 - Lists And Collections C#/Program.cs
+++ Csharpbasics/25 - Lists And Collections C#/Program.cs
@@ -16,8 +16,73 @@ namespace ClassFields_exercise_in_C_
         public Inventory() 
         {
                 items = new List<string>();
+                itemDetails = new Dictionary<string, (double, int)>();
+    }
+
+    public void AddItem(string name, double price, int stock)
+    {
+        items.Add(name);
+        itemDetails[name] = (price, stock);
+    }
+
+    public void RemoveItem(string name)
+    {
+        items.Remove(name);
+        itemDetails.Remove(name);
+        Console.WriteLine($"{name} has been removed from the inventory.");
+    }
+
+    public void UpdateStock(string name, int newStock)
+    {
+        if (itemDetails.ContainsKey(name))
+        {
+            var item = itemDetails[name];
+            itemDetails[name] = (item.price, newStock);
+            Console.WriteLine($"Stock of {name} updated to {newStock}.");
+        }
+    }
+
+    public void PrintInventory()
+    {
+        Console.WriteLine("Current Inventory:");
+        foreach (var item in items)
+        {
+            var details = itemDetails[item];
+            Console.WriteLine($"Product: {item}, Price: {details.price}, Stock: {details.stock}");
+        }
+    }
+
+    public void PrintMostExpensiveItem()
+    {
+        var mostExpensiveItem = "";
+        double highestPrice = 0;
+
+        foreach (var item in items)
+        {
+            var details = itemDetails[item];
+            if (details.price > highestPrice)
+            {
+                highestPrice = details.price;
+                mostExpensiveItem = item;
+            }
         }
 
+        Console.WriteLine($"Most expensive item: {mostExpensiveItem}, Price: {highestPrice}");
+    }
+
+    public void CalculateTotalInventoryValue()
+    {
+        double totalValue = 0;
+
+        foreach (var item in items)
+        {
+            var details = itemDetails[item];
+            totalValue += details.price * details.stock;
+        }
+
+        Console.WriteLine($"Total Inventory Value: {totalValue}");
+    }
+}
 
     internal class Program
     {
